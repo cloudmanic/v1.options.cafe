3ccbdcf3d73f19f607ead5134218ab27b5cd0f4b
Spicer Matthews <github@cloudmanic.com>
2017-08-19 21:20:42 -0700

    Got the html formatted email in place for sending reset password emails.

--- backend/library/email/send.go
+++ backend/library/email/send.go
@@ -2,9 +2,7 @@ package email
 
 import (
   "os"
-  "bytes"
   "strconv"
-  "html/template"
   "gopkg.in/gomail.v2"
   "app.options.cafe/backend/library/services"
 )
@@ -14,38 +12,16 @@ import (
 // If we have a SMTP in our configs we use that if not we use
 // Mailgun's library for sending mail. 
 //
-func Send(to string, subject string, html string) error {
-  
-	templateData := struct {
-		Name string
-		URL  string
-	}{
-		Name: "Dhanush",
-		URL:  "http://geektrust.in",
-	}  
-  
-  
-  // Build the html email we are going to send.
-  tpl := template.New(subject)
-  t, err := tpl.Parse(html)
-	
-	if err != nil {
-    return err
-	}
-  
-	buf := new(bytes.Buffer)
-	
-	if err = t.Execute(buf, templateData); err != nil {
-		return err
-	}	 
-  
+func Send(to string, subject string, html string, text string) error {
+    
   // Setup the email to send.
   m := gomail.NewMessage()
   m.SetHeader("From", "help@options.cafe")
   m.SetHeader("To", to)
   m.SetHeader("Bcc", "bcc@options.cafe")
   m.SetHeader("Subject", subject)
-  m.SetBody("text/html", buf.String())
+  m.SetBody("text/html", html)
+  m.AddAlternative("text/plain", text)
 
   // Configure the port to be an int. 
   port, _ := strconv.ParseInt(os.Getenv("MAIL_PORT"), 10, 64)
