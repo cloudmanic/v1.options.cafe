5cecd3dc634dd2b7e7de4115b907ff9565c3526f
Spicer Matthews <github@cloudmanic.com>
2017-09-04 00:34:23 -0700

    Toasted some code.

--- backend/models/connection.go
+++ backend/models/connection.go
@@ -1,40 +1,11 @@
 package models
 
 import (
-  //"os"
-  //"log"
   "github.com/jinzhu/gorm"
-  //_ "github.com/go-sql-driver/mysql"
 )
 
 type Connection struct {
   Conn *gorm.DB
 } 
-
-/*
-//
-// Connect to the db and run migrations.
-//
-func (m Connection) Connect() (*gorm.DB) {
-  
-  var err error
-    
-  // Connect to Mysql
-  m.Conn, err := gorm.Open("mysql", os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@/" + os.Getenv("DB_DATABASE") + "?charset=utf8&parseTime=True&loc=Local")
-  
-  if err != nil {
-    panic("failed to connect database")
-  }
-
-  // Enable
-  m.Conn.LogMode(true)
-  m.Conn.SetLogger(log.New(os.Stdout, "\r\n", 0))
-
-  // Migrate the schemas (one per table).
-  m.Conn.AutoMigrate(&User{})
-  
-  return m.Conn   
-}
-*/
 
 /* End File */
