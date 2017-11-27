7f499e15dea6eed144ea18023e9d653e6cc30a51
Spicer Matthews <github@cloudmanic.com>
2017-11-26 22:16:15 -0800

    Backup commit. Historical Import.

--- backend/library/import/options/bulk.go
+++ backend/library/import/options/bulk.go
@@ -7,14 +7,17 @@
 package options
 
 import (
+	"bufio"
 	"encoding/csv"
 	"errors"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/app.options.cafe/backend/library/files"
 	"github.com/app.options.cafe/backend/library/services"
+	"github.com/app.options.cafe/backend/library/store/object"
 	"github.com/araddon/dateparse"
 )
 
@@ -30,42 +33,99 @@ import (
 //
 func DoBulkEodImportToPerSymbolDay() {
 
-	err := OneMonthEodImport("/Users/spicer/Development/app.options.cafe/backend/cache/object-store/2002_April.zip")
+	// Do historical one month EOD data
+	err := DoHistoricalMonthlyEodData()
 
 	if err != nil {
 		panic(err)
 	}
 
-	// // Grab a list all historical (per month) data from our object store. (stored in bucket options-eod-monthly)
-	// // This goes from options-eod-monthly/2002_April.zip - 2017_September.zip
-	// objects, err := object.ListObjects("options-eod-monthly")
+	// Success processing.
+	services.Info("DoHistoricalMonthlyEodData Done.")
+}
 
-	// if err != nil {
-	// 	panic(err)
-	// }
+//
+// Do Historical Monthly EOD data processing.
+//
+func DoHistoricalMonthlyEodData() error {
 
-	// // Loop through each month / year zip file in options-eod-monthly.
-	// for _, row := range objects {
+	// Get a map of monthly EOD's that we have already done.
+	monthlyDone, err := ReadMonthlyEodImportLog()
 
-	// 	// Check to see if we already did this key (maybe the program died and we are resuming)
+	if err != nil {
+		return err
+	}
 
-	// 	// Download file to our cache dir.
-	// 	services.Info("Downloading: " + row.Key)
+	// Grab a list all historical (per month) data from our object store. (stored in bucket options-eod-monthly)
+	// This goes from options-eod-monthly/2002_April.zip - 2017_September.zip
+	objects, err := object.ListObjects("options-eod-monthly")
 
-	// 	path, err := object.DownloadObject(row.Key)
+	if err != nil {
+		return err
+	}
 
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
+	// Loop through each month / year zip file in options-eod-monthly.
+	for _, row := range objects {
 
-	// 	fmt.Println(path)
+		// Check to see if we already did this key (maybe the program died and we are resuming)
+		if _, ok := monthlyDone[filepath.Base(row.Key)]; ok {
+			services.Info("Skipping (already done): " + row.Key)
+			continue
+		}
 
-	// 	// Unzip the file.
+		// TODO: pass this off to a worker.
+		// Process one Month's worth of EOD data.
+		err := DoOneMonthsEodData(row.Key)
 
-	// 	// Loop through the days of each month
+		if err != nil {
+			return err
+		}
 
-	// }
+	}
 
+	// Return Happy.
+	return nil
+}
+
+//
+// Process One month's worth of EOD data.
+//
+func DoOneMonthsEodData(key string) error {
+
+	// Downalod object
+	path, err := DownloadOneMonthEodData(key)
+
+	if err != nil {
+		return err
+	}
+
+	// Process the data we just imported.
+	err = OneMonthEodImport(path)
+
+	if err != nil {
+		return err
+	}
+
+	// Return happy
+	return nil
+}
+
+//
+// Download one month's worth of EOD data.
+//
+func DownloadOneMonthEodData(key string) (string, error) {
+
+	// Download file to our cache dir.
+	services.Info("Downloading: " + key)
+
+	path, err := object.DownloadObject(key)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Return Happy.
+	return path, nil
 }
 
 //
@@ -108,6 +168,19 @@ func OneMonthEodImport(zipFilePath string) error {
 			return err
 		}
 
+	}
+
+	// Log this file is complete
+	f, err := os.OpenFile(os.Getenv("CACHE_DIR")+"/OneMonthEodImport.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(filepath.Base(zipFilePath) + "\n"); err != nil {
+		return err
 	}
 
 	// Return Happy
@@ -262,6 +335,29 @@ func OneDayEodSymbol(symbol string, date time.Time, data [][]string) (string, er
 
 	// Return happy
 	return zipFile, err
+}
+
+//
+// Read Monthly log.
+//
+func ReadMonthlyEodImportLog() (map[string]bool, error) {
+	log := make(map[string]bool)
+
+	file, err := os.Open(os.Getenv("CACHE_DIR") + "/OneMonthEodImport.log")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		log[scanner.Text()] = true
+	}
+
+	return log, scanner.Err()
 }
 
 /* End File */
