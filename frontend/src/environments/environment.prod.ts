46c4c62ca7d9fa149788a8286c055fa115129258
Spicer Matthews <github@cloudmanic.com>
2018-02-10 20:57:59 -0800

    Added a config for default page size. (limit)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   app_server: "https://app.options.cafe",
-  ws_server: "wss://app.options.cafe"  
+  ws_server: "wss://app.options.cafe",
+  default_page_size: 100    
 };
