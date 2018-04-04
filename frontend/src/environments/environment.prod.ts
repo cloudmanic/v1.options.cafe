ccce75d9dc332cb264692bd98da939f4289d2f88
Spicer Matthews <github@cloudmanic.com>
2018-04-03 21:37:05 -0700

    Updated client_id

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  client_id: "g-R76CA79BX1GjD",
+  client_id: "eLdOiC4QGy9MIkL",
   app_server: "https://app.options.cafe",
   ws_server: "wss://app.options.cafe"  
 };
