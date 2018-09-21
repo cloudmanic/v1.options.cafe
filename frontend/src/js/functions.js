46e64026287e1542518d9f1c1dd0ad952b738d49
Spicer Matthews <github@cloudmanic.com>
2018-09-21 09:18:31 -0700

    Removed mobile navigation stuff.

--- frontend/src/js/functions.js
+++ frontend/src/js/functions.js
@@ -1,52 +1,52 @@
-;(function($, window, document, undefined) {
-	var $win = $(window);
-	var $doc = $(document);
+// ;(function($, window, document, undefined) {
+// 	var $win = $(window);
+// 	var $doc = $(document);
 
-	// Set Padding Top of Content Block
-	function setContentPadding() {
-		var $content = $('.content');
-		var padding;
+// 	// Set Padding Top of Content Block
+// 	function setContentPadding() {
+// 		var $content = $('.content');
+// 		var padding;
 
-		if ($win.width() < 1320) {
-			padding = $('.banner--fixed').length ? $('.header-wrapper').outerHeight() + 50 : $('.header-wrapper').outerHeight() + 20;
-		} else {
-			padding = $('.header-wrapper').outerHeight() + 20;
-		}
+// 		if ($win.width() < 1320) {
+// 			padding = $('.banner--fixed').length ? $('.header-wrapper').outerHeight() + 50 : $('.header-wrapper').outerHeight() + 20;
+// 		} else {
+// 			padding = $('.header-wrapper').outerHeight() + 20;
+// 		}
 
-		if ($content.length) {
-			$content.css('padding-top', padding);
-		}
-	}
+// 		if ($content.length) {
+// 			$content.css('padding-top', padding);
+// 		}
+// 	}
 
-	$doc.ready(function() {
-		// Mobile Menu
-		$('.btn-menu').on('click', function(event) {
-			$(this).toggleClass('active');
-			$('.sidebar, .nav').toggleClass('open');
-			$('.content, .header').toggleClass('moved');
+// 	$doc.ready(function() {
+// 		// Mobile Menu
+// 		$('.btn-menu').on('click', function(event) {
+// 			$(this).toggleClass('active');
+// 			$('.sidebar, .nav').toggleClass('open');
+// 			$('.content, .header').toggleClass('moved');
 
-			if ($win.width() < 768) {
-				$('.sidebar .sidebar__content').scrollLock();
-			}
+// 			if ($win.width() < 768) {
+// 				$('.sidebar .sidebar__content').scrollLock();
+// 			}
 
-			event.preventDefault();
-		});
+// 			event.preventDefault();
+// 		});
 
-		// // Popup
-		// $('.popup--trigger').magnificPopup({ mainClass: 'mfp-fade' });
+// 		// // Popup
+// 		// $('.popup--trigger').magnificPopup({ mainClass: 'mfp-fade' });
 
-		// $('body').on('click', '.popup--close', function(event) {
-		// 	$.magnificPopup.close();
+// 		// $('body').on('click', '.popup--close', function(event) {
+// 		// 	$.magnificPopup.close();
 
-		// 	event.preventDefault();
-		// });
+// 		// 	event.preventDefault();
+// 		// });
 
-		// Set Padding Top of Content Block
-		setContentPadding();
-	});
+// 		// Set Padding Top of Content Block
+// 		setContentPadding();
+// 	});
 
-	$win.on('resize', function() {
-		setContentPadding();
-	});
+// 	$win.on('resize', function() {
+// 		setContentPadding();
+// 	});
 
-})(jQuery, window, document);
+// })(jQuery, window, document);
