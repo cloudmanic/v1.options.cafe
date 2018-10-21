d8da7b05ad817cfce9973450bb572531fe4fb2f9
Spicer Matthews <github@cloudmanic.com>
2018-10-20 22:13:12 -0700

    got high charts in place.

--- frontend/src/app/reports/custom-reports/custom-reports.component.ts
+++ frontend/src/app/reports/custom-reports/custom-reports.component.ts
@@ -4,6 +4,8 @@
 // Copyright: 2018 Cloudmanic Labs, LLC. All rights reserved.
 //
 
+import * as moment from 'moment-timezone';
+import * as Highcharts from 'highcharts/highstock';
 import { ProfitLoss } from '../../models/reports';
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../providers/state/state.service';
@@ -18,6 +20,78 @@ import { ReportsService } from '../../providers/http/reports.service';
 export class CustomReportsComponent implements OnInit 
 {
   listData: ProfitLoss[] = [];
+  Highcharts = Highcharts;
+
+  chartConstructor = 'chart';
+
+  chartUpdateFlag: boolean = false;
+
+  // High charts config
+  chartOptions = {
+    lang: {
+      thousandsSep: ','
+    },
+
+    chart: { type: 'column' },
+
+    title: { text: '' },
+    credits: { enabled: false },
+
+    rangeSelector: { enabled: false },
+
+    scrollbar: { enabled: false },
+
+    navigator: { enabled: false },
+
+    legend: { enabled: false },
+
+    time: {
+      getTimezoneOffset: function(timestamp) {
+        // America/Los_Angeles
+        // America/New_York
+        let timezoneOffset = -moment.tz(timestamp, 'America/Los_Angeles').utcOffset();
+        return timezoneOffset;
+      }
+    },
+
+    tooltip: {
+      formatter: function() {
+        return "<b>" + this.points[0].series.name + ": </b><br />" + Highcharts.dateFormat('%b \'%y', this.points[0].x) + " : $" + Highcharts.numberFormat(this.points[0].y, 0, '.', ',');
+      },
+
+      shared: true
+    },    
+
+    yAxis: {
+      title: {
+        text: 'Profit & Loss'
+      },
+
+      labels: {
+        formatter: function() {
+          return '$' + Highcharts.numberFormat(this.axis.defaultLabelFormatter.call(this), 0, '.', ',');
+        }
+      }
+    },    
+
+    xAxis: {
+      type: 'datetime',
+
+      dateTimeLabelFormats: {
+        month: '%b \'%y',
+        year: '%Y'
+      },
+
+      title: {
+        text: 'Date'
+      }
+    },
+
+    series: [{
+      name: 'Profit & Loss',
+      data: []
+    }]
+  };  
 
   //
   // Construct.
@@ -44,7 +118,25 @@ export class CustomReportsComponent implements OnInit
     this.reportsService.getProfitLoss(Number(this.stateService.GetStoredActiveAccountId()), "2018-01-01", "2018-12-31", "month", "desc").subscribe((res) => {
       this.listData = res;
 
-      console.log(res);
+      var data = [];
+
+      for (var i = 0; i < res.length; i++) 
+      {
+        let color = "#5cb85c";
+
+        if(res[i].Profit < 0)
+        {
+          color = "#ce4260";
+        }
+
+        data.push({ x: res[i].Date, y: res[i].Profit, color: color });
+      }
+
+      // Rebuilt the chart
+      this.chartOptions.series[0].data = data;
+      this.chartOptions.series[0].name = "Profit & Loss";
+      this.chartUpdateFlag = true;
+
     });
   }
 
