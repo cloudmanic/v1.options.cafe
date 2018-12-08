13935ea937e5ab5ef6447e7fc85f6b04c3265cbe
Spicer Matthews <github@cloudmanic.com>
2018-12-07 18:40:03 -0800

    Got backups and some other stuff working at DO

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,4 @@ cd ../ansible
 ansible-playbook app.yml
 cd ../scripts
 
-# Login as myself and build and restart
-ssh web2.cloudmanic.com "cd /sites/optionscafe/app.options.cafe/docker && docker-compose build && docker-compose down && docker-compose up -d && docker image prune -a -f"
-
 ## TODO: make an api call to papertail and output the current logs just to see if anything went wrong during deploy
